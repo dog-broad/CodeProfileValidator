31edd489fc433175f72db38c8348bbc3f049e81f
dog-broad <rushyendra.7004@gmail.com>
2024-08-25 00:43:59 +0530

    Better check url platform format

--- profile_checker/urls.py
+++ profile_checker/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import check_url_platform
 
 urlpatterns = [
-    path('check-url-platform/', check_url_platform, name='check_url_platform'),
+    path('check-url-platform/<str:platform>/<str:username>/', check_url_platform, name='check_url_platform'),
 ]
